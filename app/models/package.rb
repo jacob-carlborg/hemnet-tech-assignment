61c744ceaf3a219492dd58980b454a808e1729b5
Jacob Carlborg <doob@me.com>
2025-07-09 10:27:35 +0200

    Remove `package_id` column from `prices` table

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Package < ApplicationRecord
-  has_many :prices, dependent: :destroy
   has_many :municipalities, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
