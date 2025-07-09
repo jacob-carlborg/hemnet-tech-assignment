5a75c69f52a4153ec6be6797643b429c4a31fe28
Jacob Carlborg <doob@me.com>
2025-07-09 12:09:41 +0200

    Remove `packages.amount_cents`
    The current price is now stored for each municipality instead.

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,5 +4,4 @@ class Package < ApplicationRecord
   has_many :municipalities, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :amount_cents, presence: true
 end
